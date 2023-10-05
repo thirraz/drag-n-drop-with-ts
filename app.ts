df56e909e3b524ce5ee7c904af69fee7c48d844f
Thiago Souza <thiagofernandosouza20@gmail.com>
2023-10-05 08:06:00 -0300

    accessing DOM elements

--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement
 	hostElement: HTMLDivElement
 	element: HTMLFormElement
+	titleInputElement: HTMLInputElement
+	descriptionInputElement: HTMLInputElement
+	peopleInputElement: HTMLInputElement
 
 	constructor() {
 		this.templateElement = <HTMLTemplateElement>(
@@ -16,11 +19,40 @@ class ProjectInput {
 		)
 
 		this.element = <HTMLFormElement>importedNode.firstElementChild
+		this.element.id = "user-input"
 
+		this.titleInputElement = <HTMLInputElement>(
+			this.element.querySelector("#title")
+		)
+		this.descriptionInputElement = <HTMLInputElement>(
+			this.element.querySelector("#description")
+		)
+		this.peopleInputElement = <HTMLInputElement>(
+			this.element.querySelector("#people")
+		)
+
+		this.configure()
 		this.attach()
+	}
+
+	private gatherUserInput(): [string, string, number] {
+		const enteredTitle = this.titleInputElement.value
+		const enteredDescription = this.descriptionInputElement.value
+		const enteredPeople = this.peopleInputElement.value
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault()
+		const userInput = this.gatherUserInput()
+	}
+
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler.bind(this))
 	}
 
 	private attach() {
 		this.hostElement.insertAdjacentElement("afterbegin", this.element)
 	}
 }
+
+const prjInput = new ProjectInput()
--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,28 @@ var ProjectInput = /** @class */ (function () {
         this.hostElement = document.querySelector("#app");
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.titleInputElement = (this.element.querySelector("#title"));
+        this.descriptionInputElement = (this.element.querySelector("#description"));
+        this.peopleInputElement = (this.element.querySelector("#people"));
+        this.configure();
         this.attach();
     }
+    ProjectInput.prototype.gatherUserInput = function () {
+        var enteredTitle = this.titleInputElement.value;
+        var enteredDescription = this.descriptionInputElement.value;
+        var enteredPeople = this.peopleInputElement.value;
+    };
+    ProjectInput.prototype.submitHandler = function (event) {
+        event.preventDefault();
+        var userInput = this.gatherUserInput();
+    };
+    ProjectInput.prototype.configure = function () {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
+    };
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
     };
     return ProjectInput;
 }());
+var prjInput = new ProjectInput();
